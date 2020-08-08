5601878ee3ab99c4fa84ef222a770a9fce8fc20f
EricBruch <eric.bruch@outlook.de>
2020-08-08 20:57:55 +0200

    Added Enum for Eisenhower Categories & implemented parts of loading array for display in matrix

---
+++ overallApp.js
@@ -0,0 +1,8 @@
+const eisenhowerMatrixCategrories =
+{
+    DO: "do",
+    SCHEDULE: "schedule",
+    DELEGATE: "delegate",
+    ELIMINATE: "eliminate"
+}
+Object.freeze(eisenhowerMatrixCategrories);
