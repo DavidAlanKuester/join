c1fb198b9a037d182e5254d73a9afe3cc3a0ae8d
David Küster <davidalankuester@yahoo.de>
2020-08-10 18:19:08 +0200

    inputValues & taskJson

--- addTask.js
+++ addTask.js
@@ -13,22 +13,12 @@ function enableBtn() {
 
 function enableCancelBtn() {
     document.getElementById('cancel-btn').disabled =
-    document.getElementById('title-input').value.trim().length == 0 &&
-    document.getElementById('category-input').value.trim().length == 0 &&
-    document.getElementById('description-input').value.trim().length == 0 &&
-    document.getElementById('date-input').value.trim().length == 0 &&
-    document.getElementById('urgency-input').value.trim().length == 0 &&
-    document.getElementById('importance-input').value.trim().length == 0;
+        document.getElementById('title-input').value.trim().length == 0;
 }
 
 function enableCreateTaskBtn() {
-    document.getElementById('create-btn').disabled = 
-    document.getElementById('title-input').value.trim().length == 0 &&
-    document.getElementById('category-input').value.trim().length == 0 &&
-    document.getElementById('description-input').value.trim().length == 0 &&
-    document.getElementById('date-input').value.trim().length == 0 &&
-    document.getElementById('urgency-input').value.trim().length == 0 &&
-    document.getElementById('importance-input').value.trim().length == 0;
+    document.getElementById('create-btn').disabled =
+        document.getElementById('title-input').value.trim().length == 0;
 }
 
 // *************** Mode to enable Cancel & Create Button -End ************************
@@ -45,7 +35,7 @@ function addPerson() {
         div.appendChild(person);
         persons = persons + 0;
     }
-    if (persons <=1) {
+    if (persons <= 1) {
         document.getElementById('remove-btn-div').classList.remove('d-none');
     }
 
@@ -60,16 +50,59 @@ function addPerson() {
         removeBtn.innerHTML = "Remove";
         div.appendChild(removeBtn);
     }
-    */ 
+    */
+}
+function removePerson() {
+    persons = 0;
+    document.getElementById('assign-person-div').innerHTML = '';
+    document.getElementById('remove-btn-div').classList.add('d-none');
+
 }
 
 
 // *****Assigned To Section - Add Persons - End *********
 
+// ***** Value Add from Select to P - start ******* 
+function selectCategory() {
+    let categoryChoice = document.getElementById('category-input');
+    let displayCategoryText = categoryChoice.options[categoryChoice.selectedIndex].text;
+    document.getElementById('category-output').innerHTML = displayCategoryText;
+}
+
+function selectImportance() {
+    let importanceChoice = document.getElementById('importance-input');
+    let displayImportanceText = importanceChoice.options[importanceChoice.selectedIndex].text;
+    document.getElementById('importance-output').innerHTML = displayImportanceText;
+}
+// ***** Value Add from Select to P - End ******* 
+
+
+
+
+
+// ***** Create Task (JSON Push) - start ******* 
+// ***** Create Task (JSON Push) - End ******* 
+let tasks = [];
+let taskID = 0
 
 function createTask() {
-
+    newTask();
+ 
 }
 
 
-
+function newTask() {
+   // let personDiv = document.getElementById('assign-person-div');
+    // let Imgs = personDiv.document.getElementsByTagName('img');
+    let newTask = {
+        "task-id": taskID++,
+        "title": document.getElementById('title-input').value,
+        "category": document.getElementById('category-input').value,
+        "description": document.getElementById('description-input').value,
+        "due-date": document.getElementById('date-input').value,
+        "importance": document.getElementById('importance-input').value,
+      //  "assigned-to": Imgs
+    }
+    tasks.push(newTask)
+    console.log(newTask);
+}
