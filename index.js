3a34b63960665a1bba5ecefc279607b36660de8f
crunck78 <crunck78@googlemai.com>
2020-09-04 14:59:55 +0200

    "add user profile, not finished

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
         sidebarSetUserImg();
+		document.getElementById("loader").classList.add("d-none");
+		document.getElementById("user-profile-container").classList.remove("d-none");
+		document.getElementById("user-name").innerHTML = user.displayName;
+		
+		document.getElementById("user-email").innerHTML = user.email;
+		document.getElementById("user-img2").src = user.photoURL;
+		
     } else {
         // User is signed out.
         ui.start('#firebaseui-auth-container', uiConfig);
